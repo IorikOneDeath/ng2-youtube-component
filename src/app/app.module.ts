88ef685211f65488573084bf8cb139334401ca55
IorikOneDeath <iorikonedeath@gmail.com>
2017-02-14 17:17:19 +0200

    youtube-component was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { YoutubeCopmonentComponent } from './youtube-copmonent/youtube-copmonent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YoutubeCopmonentComponent
   ],
   imports: [
     BrowserModule,

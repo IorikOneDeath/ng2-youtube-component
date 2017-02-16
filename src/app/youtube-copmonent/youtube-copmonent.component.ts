15a1460cafd8e4cae344b944fca20ba994525665
IorikOneDeath <iorikonedeath@gmail.com>
2017-02-17 00:16:44 +0200

    'update'

--- src/app/youtube-copmonent/youtube-copmonent.component.ts
+++ src/app/youtube-copmonent/youtube-copmonent.component.ts
@@ -6,25 +6,24 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./youtube-copmonent.component.css']
 })
 export class YoutubeCopmonentComponent implements OnInit {
-  @Input() public name: string;
-           public link: string;
-           public picture: string;
-
+  public items: Array<any> = [];
   constructor() { }
-
   ngOnInit() {
-    console.log(this.name);
-    this.link = 'https://www.youtube.com/';
-    this.picture = 'file:///home/andrei/Downloads/yt.png';
+    this.items = [
+  {
+    title: 'JavaScript',
+   link: '',
+   image: 'http://www.itmathrepetitor.ru/wp-content/uploads/2015/03/JavaScript-logo.png'
+  },
+   {
+     title: 'C++',
+     link: '',
+     image: 'http://www.itmathrepetitor.ru/wp-content/uploads/2015/03/JavaScript-logo.png'
+   }
+  
+]
 
-    if (this.name === 'JS'){
-    this.link = 'https://www.youtube.com/watch?v=Ukg_U3CnJWI&t=182s';
-    this.picture = 'file:///home/andrei/Downloads/learn-javascript.png';
-    }
-    if (this.name === 'C++'){
-      this.link = 'https://www.youtube.com/watch?v=Rub-JsjMhWY';
-      this.picture = ''
-    }
+
   }
 
 }
